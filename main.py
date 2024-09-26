c2de4458cf63e73b414d5e6c1cbdf263525f00d1
kirey1412 <kireykayana14@gmail.com>
2024-09-26 20:04:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ countsatellites=0
 starttime=0
 totaltime=0
 
+
 def create_satellites():
     global starttime
     for i in range(0,numberofsatellites):
@@ -21,12 +22,32 @@ def create_satellites():
 
 
 def draw():
+    global totaltime,starttime
     screen.blit("background",(0,0))
     number=1
     for satellite in satellites:
-        number+=1
         satellite.draw()
         screen.draw.text(str(number),(satellite.pos[0],satellite.pos[1]+20))
+        number+=1
+    for line in lines:
+        screen.draw.line(line[0],line[1],"white")
+    if countsatellites<numberofsatellites:
+        totaltime=time()-starttime
+        screen.draw.text(str(round(totaltime)), (10,10), fontsize=30)
+    else:
+        screen.draw.text(str(round(totaltime)), (10,10), fontsize=30)
+
+
+def on_mouse_down(pos):
+    global countsatellites,numberofsatellites,satellites,lines
+    if countsatellites <numberofsatellites:
+        if satellites[countsatellites].collidepoint(pos):
+            if countsatellites:
+                lines.append((satellites[countsatellites-1].pos,satellites[countsatellites].pos))
+            countsatellites+=1
+        else:
+            lines=[]
+            countsatellites=0
 
 
 def update():
